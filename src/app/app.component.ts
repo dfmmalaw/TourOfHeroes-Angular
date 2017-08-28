9686c5a7da5d92007742b72195835a010bff08f7
Dean Friedland <dean.friedland@sterlingts.com>
2017-08-27 20:44:10 -0400

    Added routing service and two views

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: '<h1>{{title}}</h1><h2>{{hero}} details!</h2>'
-)}
-
+  template: `
+   <h1>{{title}}</h1>
+   <nav>
+     <a routerLink="/dashboard">Dashboard</a>
+     <a routerLink="/heroes">Heroes</a>
+   </nav>
+   <router-outlet></router-outlet>
+ `
+})
 export class AppComponent {
   title = 'Tour of Heroes';
-  hero = 'Windstorm';
 }
